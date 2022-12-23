06a6c55d9d97a041dc3a6b9e72f58b6b0c051bbe
Edilson Matola <edilsonmatola13@gmail.com>
2022-12-23 10:02:01 +0200

    Api Request file

---
+++ lib/src/data/api/api_client.dart
@@ -0,0 +1,7 @@
+class ApiClient {
+  /* 
+*Requesting currencies from the webiste using api key
+ */
+  static const request =
+      'https://api.hgbrasil.com/finance?format=json-cors&key=c6a82509';
+}
